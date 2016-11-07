71bb0c6d3eff601b25955e5f201753ef948de8f6
Donatien <donatien.gascoin@gmail.com>
2016-11-07 17:23:51 +0100

    [DGA][sw_beans] Edit Main activity and add Home activity

--- app/src/main/java/cpe/top/quizz/MainActivity.java
+++ app/src/main/java/cpe/top/quizz/MainActivity.java
@@ -1,11 +1,17 @@
 package cpe.top.quizz;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
+
+import cpe.top.quizz.Utils.UserUtils;
 
 /**
  *
@@ -16,19 +22,23 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final Button loginButton = (Button) findViewById(R.id.loginButton);
+        final TextView pseudo = (TextView) findViewById(R.id.pseudo);
+        final TextView password = (TextView) findViewById(R.id.password);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                //Intent intent = new Intent(MainActivity.this, Questionnaire.class);
-                //startActivity(intent);
+                CheckUserTask u = new CheckUserTask(MainActivity.this, pseudo.getText().toString(), password.getText().toString());
+                u.execute(pseudo.getText().toString(), password.getText().toString());
             }
         });
 
@@ -53,5 +63,45 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public class CheckUserTask extends AsyncTask<String, Integer, Boolean>
+
+    {
+        private WeakReference<MainActivity> mActivity = null;
+
+        private String pseudo, password;
+
+
+        public void link (MainActivity pActivity) {
+            mActivity = new WeakReference<MainActivity>(pActivity);
+        }
+
+        public CheckUserTask(MainActivity pActivity, String pseudo, String password){
+            this.pseudo = pseudo;
+            this.password = password;
+            link(pActivity);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... voids) {
+            if (UserUtils.userExist(pseudo, password)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute (Boolean result) {
+            if (mActivity.get() != null) {
+                if(result){
+                    Intent intent = new Intent(MainActivity.this, Home.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(mActivity.get(), "Erreur login/password", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
     }
 }

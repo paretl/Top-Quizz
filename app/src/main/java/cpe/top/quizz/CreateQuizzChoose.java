b9d4b6424ba6e67861434d889906489a10360354
Romain Chazottier <romainchazottier@gmail.com>
2016-11-08 17:00:36 +0100

    [RCR][0.1/sw_quizz_create]modified create_quizz/create_quizz_choose
    ->BackPressed for the timer(create_quizz_choose)
    -> Checking before validate
    -> getParam in create_quizz_choose

--- app/src/main/java/cpe/top/quizz/CreateQuizzChoose.java
+++ app/src/main/java/cpe/top/quizz/CreateQuizzChoose.java
@@ -3,13 +3,24 @@ package cpe.top.quizz;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CreateQuizzChoose extends AppCompatActivity {
+
+    Button validate = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_quizz_choose);
+
+        validate = (Button)findViewById(R.id.validate);
+        validate.setOnClickListener(validateListener);
+
+        //TODO : Afficher questions avec checkbox
     }
 
     public void onBackPressed() {
@@ -17,6 +28,21 @@ public class CreateQuizzChoose extends AppCompatActivity {
         Intent intent = new Intent(CreateQuizzChoose.this, CreateQuizz.class);
         intent.putExtra("name", getIntent().getStringExtra("name"));
         intent.putExtra("theme", getIntent().getIntExtra("theme", 0));
+        intent.putExtra("timer", getIntent().getIntExtra("timer", 1));
         startActivity(intent);
     }
+
+    private View.OnClickListener validateListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            String name = getIntent().getStringExtra("name");
+            int theme = getIntent().getIntExtra("theme", 0);
+            int timer = getIntent().getIntExtra("timer", 1);
+
+            //TODO : Vérif si au moins une question cochée
+            //TODO : Envoie API
+
+        }
+    };
 }

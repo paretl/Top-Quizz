617d0f49164946a9f31020d38cf3a62cfc380371
lparet <louis.paret@gmail.com>
2016-11-29 23:46:55 +0100

    [LPT][sw_Question] Add activity to choose theme before create the question

--- app/src/main/java/cpe/top/quizz/ChooseTheme.java
+++ app/src/main/java/cpe/top/quizz/ChooseTheme.java
@@ -1,22 +1,18 @@
 package cpe.top.quizz;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 import cpe.top.quizz.asyncTask.GetAllThemesTask;
 import cpe.top.quizz.asyncTask.responses.AsyncUserResponse;
+import cpe.top.quizz.beans.Question;
 import cpe.top.quizz.beans.ReturnObject;
 import cpe.top.quizz.beans.Theme;
 import cpe.top.quizz.beans.User;
@@ -27,33 +23,41 @@ import cpe.top.quizz.utils.ListViewAdapterThemes;
  */
 
 public class ChooseTheme extends AppCompatActivity implements SearchView.OnQueryTextListener, AsyncUserResponse {
-
     private static final String USER = "USER";
     private static final String THEME = "THEME";
+    private static final String STATE = "STATE";
+    private static final String QUIZZNAME = "QUIZZNAME";
+    private static final String QUESTIONS = "QUESTIONS";
+    private static final String RANDOM = "RANDOM";
 
-    private User connectedUser = null;
+    private User connectedUser = new User();
+    private String state;
 
     ListViewAdapterThemes adapter;
     SearchView editsearch;
     private ListView list;
 
     // List of themes - to add multiple themes
-    ArrayList<Theme> myThemes = new ArrayList<>();
+    private ArrayList<Theme> myThemes = new ArrayList<>();
+    // List of questions already choosed - only for createTheme
+    private ArrayList<Question> myQuestions = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_theme);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        myToolbar.setTitleTextColor(Color.WHITE);
-        setSupportActionBar(myToolbar);
 
         Intent intent = getIntent();
-        if (intent != null) {
-            connectedUser = (User) intent.getSerializableExtra(USER);
-            if(intent.getSerializableExtra(THEME) != null){
-                myThemes = (ArrayList<Theme>) intent.getSerializableExtra(THEME);
-            }
+        if(intent != null) {
+            myThemes = (ArrayList<Theme>) intent.getSerializableExtra(THEME);
+            state = intent.getStringExtra(STATE);
+            myQuestions =  (ArrayList<Question>) intent.getSerializableExtra(QUESTIONS);
+        }
+
+        // change the title
+        TextView title = (TextView) findViewById(R.id.title);
+        if("Quizz".equals(state)) {
+            title.setText("Choisis le thème de ton quizz");
         }
 
         // AsyncTask to take all Themes
@@ -91,9 +95,9 @@ public class ChooseTheme extends AppCompatActivity implements SearchView.OnQuery
                             resultsList.add(t);
                         }
                     }
-                } else {
-                    resultsList.add(t);
                 }
+            } else {
+                resultsList.add(t);
             }
         }
 
@@ -101,14 +105,11 @@ public class ChooseTheme extends AppCompatActivity implements SearchView.OnQuery
         Intent intent = getIntent();
         if (intent != null) {
             connectedUser = (User) intent.getSerializableExtra(USER);
-            if(connectedUser == null) {
-                Intent i = new Intent(ChooseTheme.this, MainActivity.class);
-                startActivity(i);
-            }
+            state = intent.getStringExtra(STATE);
         }
 
         // Pass results to ListViewAdapter Class
-        adapter = new ListViewAdapterThemes(this, resultsList, connectedUser, myThemes);
+        adapter = new ListViewAdapterThemes(this, resultsList, connectedUser, myThemes, state, intent.getStringExtra(QUIZZNAME), intent.getIntExtra(RANDOM, 0), myQuestions);
 
         list = (ListView) findViewById(R.id.listViewTheme);
         // Binds the Adapter to the ListView
@@ -117,33 +118,5 @@ public class ChooseTheme extends AppCompatActivity implements SearchView.OnQuery
         // Locate the EditText in activity_choose_theme
         editsearch = (SearchView) findViewById(R.id.searchView);
         editsearch.setOnQueryTextListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.settings:
-                Toast.makeText(this, "Settings selected", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.logout:
-                // Destroy user and return to main activity
-                connectedUser = null;
-                Toast.makeText(this, "A bientôt !", Toast.LENGTH_LONG).show();
-                Intent intent = new Intent(ChooseTheme.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-                break;
-            default:
-                break;
-        }
-        return true;
     }
 }

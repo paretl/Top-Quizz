86152514e4c7e195b02a6208ca74ea1f4dede33a
Camille Cordier <cordier.camille.cc@gmail.com>
2016-12-27 16:11:05 +0100

    [CCR][0.1/statsArray] Debut de création de la page des stats sous forme de tableau.

--- app/src/main/java/cpe/top/quizz/StatsArray.java
+++ app/src/main/java/cpe/top/quizz/StatsArray.java
@@ -1,13 +1,114 @@
 package cpe.top.quizz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import cpe.top.quizz.beans.User;
+
+/**
+ * Created by Camille on 20/12/2016.
+ */
 
 public class StatsArray extends AppCompatActivity {
+
+    private static final String USER = "USER";
+    //private static final String QUIZZ = "QUIZZ";
+
+    private User connectedUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats_array);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        myToolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(myToolbar);
+
+        //List<Results> listQ = null; //on veut une liste de resultats au quizz
+/*
+        Intent intent = getIntent();
+        if (intent != null && getIntent().getSerializableExtra(LIST_QUIZZ) != null) { //aremplacer par listRestlts
+            connectedUser = (User) getIntent().getSerializableExtra(USER);
+            // The list (IHM)
+            ListView list = (ListView) findViewById(R.id.listResults);
+
+        }
+*/
+        arrayStats();
+    }
+
+    private void arrayStats() {
+        // données du tableau
+        final String [] col1 = {"col1:ligne1","col1:ligne2","col1:ligne3","col1:ligne4","col1:ligne5"};
+        final String [] col2 = {"col2:ligne1","col2:ligne2","col2:ligne3","col2:ligne4","col2:ligne5"};
+
+        //TableLayout table = (TableLayout) findViewById(R.id.idTable); // on prend le tableau défini dans le layout
+        TableRow row; // création d'un élément : ligne
+        TextView tv1,tv2; // création des cellules
+
+        // pour chaque ligne
+        for(int i=0;i<col1.length;i++) {
+            row = new TableRow(this); // création d'une nouvelle ligne
+
+            tv1 = new TextView(this); // création cellule
+            tv1.setText(col1[i]); // ajout du texte
+            tv1.setGravity(Gravity.CENTER); // centrage dans la cellule
+            // adaptation de la largeur de colonne à l'écran :
+            tv1.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
+
+            // idem 2ème cellule
+            tv2 = new TextView(this);
+            tv2.setText(col2[i]);
+            tv2.setGravity(Gravity.CENTER);
+            tv2.setLayoutParams( new TableRow.LayoutParams( 0, android.view.ViewGroup.LayoutParams.WRAP_CONTENT, 1 ) );
+
+            // ajout des cellules à la ligne
+            row.addView(tv1);
+            row.addView(tv2);
+
+            // ajout de la ligne au tableau
+            //table.addView(row);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.logout:
+                // Destroy user and return to main activity
+                Toast.makeText(this, "A bientôt !", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(StatsArray.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
--- app/src/main/java/cpe/top/quizz/StatsArrays.java
+++
@@ -1,8 +0,0 @@
-package cpe.top.quizz;
-
-/**
- * Created by Camille on 20/12/2016.
- */
-
-public class StatsArrays {
-}
